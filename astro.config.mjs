ac609e11bb87c35f3f776d06195a4b4955fde98b
Valentin Misiaszek <mowerv6@gmail.com>
2026-01-08 00:23:08 +0100

    feat: improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://valentin.webxfactory.fr', // ✅ Sous-domaine
+    site: 'https://valentin.webxfactory.fr',
     output: 'static',
+    integrations: [sitemap()],
     build: {
         format: 'directory'
     }
